d23932fbb9e4189bd68019c00c8b7c5ddb3f6609
Scott Schindler <scottschindler29@gmail.com>
2020-05-11 21:07:55 -0400

    day 1

---
+++ cpu-play.py
@@ -0,0 +1,90 @@
+
+"""CPU functionality."""
+
+import sys
+
+
+class CPU:
+    """Main CPU class."""
+
+    def __init__(self):
+        """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.sp = 0
+        self.pc = 0
+        self.FL = 0
+
+    def load(self):
+        """Load a program into memory."""
+
+        address = 0
+
+        # For now, we've just hardcoded a program:
+
+        program = [
+            # From print8.ls8
+            0b10000010,  # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b01000111,  # PRN R0
+            0b00000000,
+            0b00000001,  # HLT
+        ]
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        # elif op == "SUB": etc
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, index):
+        print(self.ram[index])
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            # self.fl,
+            # self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+
+    def run(self):
+        """Run the CPU."""
+        running = True
+        while running == True:
+            command = self.ram[self.pc]
+            # LDI
+            if command == 0b10000010:
+                self.reg[self.pc + 1] = self.ram[self.pc + 2]
+                self.pc += 3
+            # PRINT
+            elif command == 0b01000111:
+                print(self.ram[self.pc + 1])
+                self.pc += 2
+            # HALT
+            elif command == 0b00000001:
+                running = False
+            # IF NOT RECOGNIZED
+            else:
+                sys.exit(1)
+
